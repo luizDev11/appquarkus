27a0012583801b225a286c51c3c000531553e4f5
luizDev11 <luiz357hunter@gmail.com>
2026-05-09 22:59:03 -0300

    CRUD de cliente

--- src/main/java/br/com/vivistudio/resource/ClienteResource.java
+++ src/main/java/br/com/vivistudio/resource/ClienteResource.java
@@ -1,8 +1,9 @@
 package br.com.vivistudio.resource;
 
+import jakarta.transaction.Transactional;
 import br.com.vivistudio.entity.Cliente;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
 
 import java.util.List;
 
@@ -13,4 +14,69 @@ public class ClienteResource {
     public List<Cliente> listar() {
         return Cliente.listAll();
     }
+
+    @GET
+    @Path("/{id}")
+    public Response buscarPorId(@PathParam("id") Long id) {
+        Cliente cliente = Cliente.findById(id);
+
+        if (cliente == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.ok(cliente).build();
+    }
+
+    @POST
+    @Transactional
+    @Path("/criar")
+    public Response criarCliente(Cliente cliente) {
+
+        if (cliente.nome == null || cliente.telefone == null) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Nome e Telefone devem ser obrigatórios!")
+                    .build();
+        }
+        cliente.persist();
+
+        return Response.status(Response.Status.CREATED)
+                .entity("Cliente criado!")
+                .build();
+
+    }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response atualizarCliente(
+            @PathParam("id") long id,
+            Cliente dados) {
+        Cliente cliente = Cliente.findById(id);
+
+        if (cliente == null) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Cliente não encontrado")
+                    .build();
+        }
+        cliente.nome = dados.nome;
+        cliente.telefone = dados.telefone;
+
+        return Response.ok(cliente).build();
+    }
+
+
+    @DELETE
+    @Transactional
+    @Path("/{id}")
+    public Response deletarCliente(@PathParam("id") Long id) {
+        boolean deletado = Cliente.deleteById(id);
+
+        if (!deletado) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .entity("Cliente não encontrado")
+                    .build();
+        }
+
+        return Response.noContent().build();
+    }
+
 }
